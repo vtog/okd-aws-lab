530cc5634b3a6d1768a2a59272326c0638f02a94
vtog <v.tognaci@f5.com>
2021-05-27 09:14:08 -0500

    created vpc objects and testing

--- main.tf
+++ main.tf
@@ -12,19 +12,73 @@ resource "aws_vpc" "lab_vpc" {
 
   tags = {
     Name = "lab_vpc"
-    Lab  = "Containers"
+    Lab  = "okd4"
+  }
+}
+
+# Subnets
+
+resource "aws_subnet" "public1_subnet" {
+  vpc_id                  = aws_vpc.lab_vpc.id
+  cidr_block              = var.cidrs["public1"]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "public1"
+    Lab  = "okd4"
+  }
+}
+
+resource "aws_subnet" "private1_subnet" {
+  vpc_id                  = aws_vpc.lab_vpc.id
+  cidr_block              = var.cidrs["private1"]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "private1"
+    Lab  = "okd4"
+  }
+}
+
+# Allocate EIP
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  
+  tags = {
+    Name = "nat_eip"
+    Lab  = "okd4"
   }
 }
 
 # Internet gateway
 
-resource "aws_internet_gateway" "lab_internet_gateway" {
+resource "aws_internet_gateway" "lab_internet_gw" {
   vpc_id = aws_vpc.lab_vpc.id
 
   tags = {
     Name = "lab_igw"
-    Lab  = "Containers"
+    Lab  = "okd4"
   }
+}
+
+# NAT gateway
+
+resource "aws_nat_gateway" "lab_nat_gw" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id     = aws_subnet.public1_subnet.id
+
+    depends_on = [
+        aws_internet_gateway.lab_internet_gw,
+        aws_eip.nat_eip
+    ]
+
+    tags = {
+        Name = "lab_nat"
+        Lab  = "okd4"
+    }
 }
 
 # Route tables
@@ -34,50 +88,56 @@ resource "aws_route_table" "lab_public_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.lab_internet_gateway.id
+    gateway_id = aws_internet_gateway.lab_internet_gw.id
   }
 
   tags = {
     Name = "lab_public"
-    Lab  = "Containers"
+    Lab  = "okd4"
   }
 }
 
-# Subnets
+resource "aws_route_table" "lab_private_rt" {
+  vpc_id = aws_vpc.lab_vpc.id
 
-resource "aws_subnet" "mgmt_subnet" {
-  vpc_id                  = aws_vpc.lab_vpc.id
-  cidr_block              = var.cidrs["mgmt"]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.lab_nat_gw.id
+  }
 
   tags = {
-    Name = "lab_mgmt"
-    Lab  = "Containers"
+    Name = "lab_public"
+    Lab  = "okd4"
   }
 }
 
-resource "aws_subnet" "okd_subnet" {
-  vpc_id                  = aws_vpc.lab_vpc.id
-  cidr_block              = var.cidrs["okd"]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0]
-
-  tags = {
-    Name = "lab_okd"
-    Lab  = "Containers"
-  }
-}
-
-resource "aws_route_table_association" "lab_mgmt_assoc" {
-  subnet_id      = aws_subnet.mgmt_subnet.id
+resource "aws_route_table_association" "public1_assoc" {
+  subnet_id      = aws_subnet.public1_subnet.id
   route_table_id = aws_route_table.lab_public_rt.id
 }
 
-resource "aws_route_table_association" "lab_okd_assoc" {
-  subnet_id      = aws_subnet.okd_subnet.id
-  route_table_id = aws_route_table.lab_public_rt.id
+resource "aws_route_table_association" "private1_assoc" {
+  subnet_id      = aws_subnet.private1_subnet.id
+  route_table_id = aws_route_table.lab_private_rt.id
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #----- Set default SSH key pair -----
 
@@ -88,30 +148,30 @@ resource "aws_key_pair" "lab_auth" {
 
 #----- Deploy Services -----
 
-module "svc" {
-  source      = "./svc"
-  aws_region  = var.aws_region
-  aws_profile = var.aws_profile
-  myIP        = "${chomp(data.http.myIP.body)}/32"
-  key_name    = var.key_name
-  vpc_id      = aws_vpc.lab_vpc.id
-  vpc_cidr    = var.vpc_cidr
-  vpc_subnet  = [aws_subnet.mgmt_subnet.id]
-}
+#module "svc" {
+#  source      = "./svc"
+#  aws_region  = var.aws_region
+#  aws_profile = var.aws_profile
+#  myIP        = "${chomp(data.http.myIP.body)}/32"
+#  key_name    = var.key_name
+#  vpc_id      = aws_vpc.lab_vpc.id
+#  vpc_cidr    = var.vpc_cidr
+#  vpc_subnet  = [aws_subnet.mgmt_subnet.id]
+#}
 
 #----- Deploy OpenShift -----
 
-module "okd" {
-  source           = "./okd"
-  aws_region       = var.aws_region
-  aws_profile      = var.aws_profile
-  myIP             = "${chomp(data.http.myIP.body)}/32"
-  key_name         = var.key_name
-  instance_type    = var.okd_instance_type
-  okd_master_count = var.okd_master_count
-  okd_node_count   = var.okd_node_count
-  vpc_id           = aws_vpc.lab_vpc.id
-  vpc_cidr         = var.vpc_cidr
-  vpc_subnet       = [aws_subnet.mgmt_subnet.id]
-}
+#module "okd" {
+#  source           = "./okd"
+#  aws_region       = var.aws_region
+#  aws_profile      = var.aws_profile
+#  myIP             = "${chomp(data.http.myIP.body)}/32"
+#  key_name         = var.key_name
+#  instance_type    = var.okd_instance_type
+#  okd_master_count = var.okd_master_count
+#  okd_node_count   = var.okd_node_count
+#  vpc_id           = aws_vpc.lab_vpc.id
+#  vpc_cidr         = var.vpc_cidr
+#  vpc_subnet       = [aws_subnet.mgmt_subnet.id]
+#}
 
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 #--------root/outputs.tf--------
-output "OKD-Services_IPs" {
-  value = module.svc.public_ip
-}
+#output "OKD-Services_IPs" {
+#  value = module.svc.public_ip
+#}
 
-output "OKD-Master_IPs" {
-  value = module.okd.master-public_ip
-}
+#output "OKD-Master_IPs" {
+#  value = module.okd.master-public_ip
+#}
 
-output "OKD-Worker_IPs" {
-  value = module.okd.worker-public_ip
-}
+#output "OKD-Worker_IPs" {
+#  value = module.okd.worker-public_ip
+#}
 
