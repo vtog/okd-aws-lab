8a3a8a5acb38db459ed0c415e9280ce562e6de05
vtog <v.tognaci@f5.com>
2021-06-09 18:45:57 -0500

    wrong test region; added route53 rights

--- okd/okd.tf
+++ okd/okd.tf
@@ -175,6 +175,11 @@ resource "aws_iam_role" "bootstrap-iam-role" {
           Resource = "*"
         },
         {
+          Action   = ["route53:*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+        {
           Action   = ["kms:DescribeKey"]
           Effect   = "Allow"
           Resource = "*"
